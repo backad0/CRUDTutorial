eb694d42ad8bad495eb64eac9e339e515f364534
backad0 <91736145+backad0@users.noreply.github.com>
2023-11-12 14:39:25 +0600

    Initial commit

---
+++ src/test/java/com/main/crudtutorial/CrudTutorialApplicationTests.java
@@ -0,0 +1,14 @@
+package com.main.crudtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+class CrudTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
